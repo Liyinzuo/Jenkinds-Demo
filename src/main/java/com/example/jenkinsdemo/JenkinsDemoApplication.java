88954da1c6c4297824f20e9f76633b5320fa92c7
LYZ <lyzlikebeef@163.com>
2021-09-29 17:14:27 +0800

    FIX: jenkins自动集成部署测试

--- src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,12 +2,21 @@ package com.example.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
     }
 
+
+    @GetMapping({"test"})
+    public String test() {
+
+        return "Jenkins Test success";
+    }
 }
